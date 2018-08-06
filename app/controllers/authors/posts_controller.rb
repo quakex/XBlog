fecd6495f1b1c418959c9d711c234daf41477509
quakex <iquakex@gmail.com>
2018-08-06 14:22:35 +0800

    finish part7: publish

--- app/controllers/authors/posts_controller.rb
+++ app/controllers/authors/posts_controller.rb
@@ -2,7 +2,7 @@
 module Authors
 
   class PostsController < AuthorController
-    before_action :set_post, only: [:show, :edit, :update, :destroy]
+    before_action :set_post, only: [:show, :edit, :update, :destroy, :publish, :unpublish]
 
     # GET /posts
     # GET /posts.json
@@ -24,6 +24,15 @@ module Authors
     def edit
     end
 
+    def publish
+      @post.publish
+      redirect_to authors_posts_url
+    end
+
+    def unpublish
+      @post.unpublish
+      redirect_to authors_posts_url
+    end
     # POST /posts
     # POST /posts.json
     def create
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,41 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id               :integer          not null, primary key
+#  title            :string
+#  body             :text
+#  description      :text
+#  slug             :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  banner_image_url :string
+#  author_id        :integer
+#  published        :boolean          default(FALSE)
+#  published_at     :datetime
+#
+
 class Post < ApplicationRecord
 
   extend FriendlyId
   friendly_id :title, use: :slugged
   belongs_to :author
-  scope :most_recent, -> {order(id: :desc)}
+
+  scope :most_recent, -> {order(published_at: :desc)}
+  scope :published, -> { where(published: true) }
 
   def should_generate_new_friendly_id?
     title_changed?
   end
 
+  def publish
+    update(published: true, published_at: Time.now)
+  end
+
+  def unpublish
+    update(published: false, published_at: nil)
+  end
   def display_day_published
-    "Published #{created_at.strftime('%-b %-d, %Y')}"
+    "Published #{published_at.strftime('%-b %-d, %Y')}"
   end
 end
